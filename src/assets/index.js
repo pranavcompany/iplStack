a69d8002030789737177fae53654b001d2c67b25
JAISWAL007 <AJ107610@GMAIL.COM>
2018-04-07 12:52:14 +0530

    change all image

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,9 +4,9 @@ const Assets = {
     DD : require('./DD.png'),
     CSK : require('./CSK.png'),
     KXIP : require('./KXIP.png'),
-    KKR : require('./KKR.png'),
+    KKR : require('./KKR.jpg'),
     RCB : require('./RCB.png'),
-    SRH : require('./SRH.png'),
+    SRH : require('./SRH.jpg'),
     RR : require('./RR.png'),
     TBD : require('./TBD.png'),
 }
